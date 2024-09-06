a1f4f46fd11f9747a5a4d70d6071956743771121
rohat güneş <rohatgns44@gmail.com>
2024-09-06 14:50:41 +1000

    Add project files.

---
+++ Linq-Join/AuthorBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Join { 
+
+
+
+class Author
+{
+
+    public int AuthorId { get; set; } // yazarın benzersiz kimliği
+    public string Name { get; set; }  // yazarın adı
+
+}
+
+class Book
+{
+    public int BookId { get; set; } // kitabın benzersiz kimliği 
+    public string Title { get; set; }
+    public int AuthorId { get; set; }
+
+}
+
+}
---
+++ Linq-Join/Program.cs
@@ -0,0 +1,43 @@
+﻿using Linq_Join;
+
+class Program
+{
+    static void Main()
+    {
+        List<Author> authors = new List<Author>
+        {
+            new Author { AuthorId = 1, Name = "Orhan Pamuk" },
+            new Author { AuthorId = 2, Name = "Elif Şafak" },
+            new Author { AuthorId = 3, Name = "Ahmet Ümit" },
+            new Author {AuthorId =4, Name ="Nikolav Vasilyevic"},
+            new Author {AuthorId =5, Name="Stefan ZWeıg"}
+        };
+
+        List<Book> books = new List<Book>
+        {
+            new Book { BookId = 1, Title = "Masumiyet Müzesi", AuthorId = 1 },
+            new Book { BookId = 2, Title = "Benim Adım Kırmızı", AuthorId = 1 },
+            new Book { BookId = 3, Title = "Aşk", AuthorId = 2 },
+            new Book { BookId = 4, Title = "İstanbul Hatırası", AuthorId = 3 },
+            new Book { BookId = 5, Title = "Satranc", AuthorId = 4 }
+        };
+
+
+
+        var query = from book in books
+                    join author in authors on book.AuthorId equals author.AuthorId//LINQ sorgusu ile kitapları ve yazarları birleştiriyoruz
+                    select new
+                    {
+                        Booktitle = book.Title,
+                        Authorname = author.Name
+
+                    };
+
+
+        foreach (var item in query)
+        {
+            Console.WriteLine($"Kitap: {item.Booktitle},  yazar : {item.Authorname}");
+        }
+
+    }
+}
